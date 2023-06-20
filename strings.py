cd525b28605eaaab99d82c7285414b331076e4d5
Diego Alves <diego.alves@wapstore.com.br>
2023-06-19 22:00:16 -0300

    Introdução a manipulação de strings com python

---
+++ strings.py
@@ -0,0 +1,12 @@
+# Exemplo de programa que aborda o tipo de dado string em Python
+
+frase = "Olá, Mundo!"
+#Declaração de uma string
+print("String original: ");
+print(frase);
+print();
+
+#Acessando caracteres individuais
+print("Acessando caracteres individuais: ");
+print("Primeiro caractere: ", frase[0]);
+print("Último caractere: ", frase[-1]);
