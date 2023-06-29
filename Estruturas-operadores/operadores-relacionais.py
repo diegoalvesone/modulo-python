ac023840017443c9b357f0dae067b76a9d101dbe
Diego Alves <diego.alves@wapstore.com.br>
2023-06-29 20:23:52 -0300

    Commit com exemplos práticos de operadores relacionais em Python

---
+++ Estruturas-operadores/operadores-relacionais.py
@@ -0,0 +1,25 @@
+"""
+Operadores relacionais
+"""
+x = 5;
+y = 7;
+
+#Maior que
+print(x > y); #False
+print()
+#Menor que
+print(x < y) #True
+print();
+#Igualdade
+print(x == y,"\n") #False
+
+#Maior ou igual a
+print(x >= y); #False
+print()
+#Menor ou igual a
+print(x <= y); #True
+print()
+#Diferente de
+print(x != y); #True
+print();
+
