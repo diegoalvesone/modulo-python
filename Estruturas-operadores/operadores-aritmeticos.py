dcaa8193cec610b87338ccb79c9839fdbe39eb1b
Diego Alves <diego.alves@wapstore.com.br>
2023-06-29 19:46:57 -0300

    Commit final do arquivo com exemplos práticos sobre operadores aritméticos em Python

---
+++ Estruturas-operadores/operadores-aritmeticos.py
@@ -0,0 +1,23 @@
+"""
+Operadores Aritméticos
+--------------------
+"""
+
+a = 10;
+b = 3;
+
+soma            = a + b;
+subtracao       = a - b;
+multiplicacao   = a * b;
+divisao         = a / b;
+resto           = a % b;
+potencia        = a ** b;
+restoDescartado = a // b;
+
+print("Soma: ", soma);
+print("Subtração: ", subtracao);
+print("Multiplicação: ", multiplicacao);  
+print("Divisão: ", divisao);
+print("Potência: ", potencia);
+print("Resto da divisão: ", resto);
+print("Divisão após descartartar a parte fracionária: ", restoDescartado);
